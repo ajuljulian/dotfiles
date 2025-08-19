3e770805b27a651c9752eed3cc20cfb9e8d8aae6
ajuljulian <ajuljulian@yahoo.com>
2025-08-18 22:42:38 -0700

    Updates plugin versions and enables blink.cmp
    Updates plugin versions in lazy-lock.json to their latest commits.
    
    Enables the blink.cmp completion plugin and configures copilotchat to hide ghost text when blink menu is open. This prevents visual clutter when using the completion menu.

--- nvim/lua/config/plugins/copilotchat.lua
+++ nvim/lua/config/plugins/copilotchat.lua
@@ -1,3 +1,4 @@
+-- lua/config/plugins/copilotchat.lua
 return {
 	{
 		"zbirenbaum/copilot.lua",
@@ -10,18 +11,31 @@ return {
 				},
 				panel = { enabled = true },
 			})
+
+			-- hide Copilot ghost text when blink menu is open
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "BlinkCmpMenuOpen",
+				callback = function()
+					vim.b.copilot_suggestion_hidden = true
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "BlinkCmpMenuClose",
+				callback = function()
+					vim.b.copilot_suggestion_hidden = false
+				end,
+			})
 		end,
 	},
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
 		dependencies = {
-			"zbirenbaum/copilot.lua", -- REQUIRED
-			"nvim-lua/plenary.nvim", -- REQUIRED
+			"zbirenbaum/copilot.lua",
+			"nvim-lua/plenary.nvim",
 		},
 		config = function()
-			require("CopilotChat").setup({
-				-- optional config here
-			})
+			require("CopilotChat").setup({})
 		end,
 	},
 }
