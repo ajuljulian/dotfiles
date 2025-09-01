00a1785a2e3552b00838493ee57fac22872d1c9a
ajuljulian <ajuljulian@yahoo.com>
2025-08-31 21:06:31 -0700

    Extract gitsigns configuration into separate module
      - Move gitsigns.nvim configuration from plugins.lua to dedicated gitsigns.lua file
      - Add comprehensive key mappings for git operations (stage, reset, preview, blame, diff)
      - Configure custom signs for different git states (add, change, delete, staged)
      - Enable staging area signs and hunk navigation
      - Update plugin versions in lazy-lock.json

--- nvim/lua/config/plugins/plugins.lua
+++ nvim/lua/config/plugins/plugins.lua
@@ -28,17 +28,4 @@ return {
 	{
 		"tpope/vim-fugitive",
 	},
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({
-				on_attach = function(bufnr)
-					local gs = package.loaded.gitsigns
-					local map = vim.keymap.set
-					map("n", "<leader>hp", gs.preview_hunk, { buffer = bufnr, desc = "Preview Git hunk" })
-					-- you can also map other gitsigns features here if you want
-				end,
-			})
-		end,
-	},
 }
